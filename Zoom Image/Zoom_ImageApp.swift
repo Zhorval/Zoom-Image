d73121430f9e6c1705e07ade1a425cdf14c16751
Zhorval <pablo.cebrian@hotmail.com>
2023-03-21 11:10:17 +0100

    Initial Commit

---
+++ Zoom Image/Zoom_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zoom_ImageApp.swift
+//  Zoom Image
+//
+//  Created by Pablo  on 21/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Zoom_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
